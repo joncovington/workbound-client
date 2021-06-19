bc13d31f26eebaf32c888b56b7e1a041967736cb
Jon Covington <jon.covington@gmail.com>
2021-06-19 12:26:33 -0600

    Added first test for App component.

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+import reducers from '../../reducers';
+
+
+const store = createStore(
+  reducers,
+  applyMiddleware(reduxThunk)
+);
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  ReactDOM.unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('shows the header', () => {
+
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      container
+    );
+
+    expect(container.innerHTML).toContain('<div class="ui menu"><a class="item" href="/">Workbound</a>')
+});
